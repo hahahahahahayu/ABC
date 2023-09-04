07d9263e27ab455623ea685599b918ebe76b8601
hahahahahahayu <143996874+hahahahahahayu@users.noreply.github.com>
2023-09-04 15:17:55 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include<iostream>
+using namespace std;
+int GCD(int a, int b) {
+	if (a%b == 0) {
+		return b;
+	}
+	else {
+		return GCD(b, a%b);
+	}
+}
+int main()
+{
+	cout << "輸入兩個數" << endl;
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+
+	int result = GCD(a, b);
+	printf("%d\n", result);
+	return 0;
+
+	
+}
